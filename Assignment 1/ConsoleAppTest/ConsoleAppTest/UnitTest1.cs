e3a872512d1742e935eef8a9c394ad3ce5b7533a
muhammmad-shahab <shahab.rana@gmail.com>
2016-09-06 00:04:10 +0930

    Add files via upload

---
+++ Assignment 1/ConsoleAppTest/ConsoleAppTest/UnitTest1.cs
@@ -0,0 +1,136 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ConsoleAppTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void InsertFirstWordTest()
+        {
+            //check for the functionalaty of the get count method
+            ConsoleApplication application = new ConsoleApplication();
+
+            application.Insert("abc");
+            int expected = 1;
+            int actual = application.Getcountof("abc");
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void InsertSameWordTest()
+        {
+            //check for the functionalaty of the get count method if same word inserted
+            ConsoleApplication application = new ConsoleApplication();
+
+            application.Insert("abc");
+            application.Insert("abc");
+            int expected = 2;
+            int actual = application.Getcountof("abc");
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void GetSortedStringTest()
+        {
+            //check for the functionalaty of the get count method if same word inserted
+            ConsoleApplication application = new ConsoleApplication();
+
+         
+            string expected = "apw";
+            string actual = application.GetAlphabaticalString("paw");
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void AddStringTest()
+        {
+            //check for the functionalaty of the get count method if same word inserted
+            ConsoleApplication application = new ConsoleApplication();
+
+            string first = "abce";
+            string second = "abcd";
+            string expected = first;
+            application.AddToDictionaryWordHolder(first);
+            
+            string actual = application.GetfromDictionaryWordHolder(expected);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void AddMultipleStringTest()
+        {
+            //check for the functionalaty of the get count method if same word inserted
+            ConsoleApplication application = new ConsoleApplication();
+
+            string first = "abce";
+            string second = "aebc";
+            string expected = first  + ", " + second;
+            application.AddToDictionaryWordHolder(first);
+            application.AddToDictionaryWordHolder(second);
+            string actual = application.GetfromDictionaryWordHolder(first);
+
+            Assert.AreEqual(expected, actual);
+            System.Console.WriteLine(first);
+        }
+
+        [TestMethod]
+        public void CountTest()
+        {
+            ConsoleApplication application = new ConsoleApplication();
+
+            int expected = 3;
+
+            application.GroupWordsAndCount();
+
+            int actual = application.GetCount("apw");
+
+            Assert.AreEqual(expected, actual);
+            
+        }
+
+        [TestMethod]
+        public void GetMaxTest()
+        {
+            ConsoleApplication application = new ConsoleApplication();
+
+            string expected = "apw";
+
+            application.GroupWordsAndCount();
+
+            string actual = application.GetMaxKey();
+
+            Assert.AreEqual(expected, actual);
+
+        }
+
+        [TestMethod]
+        public void GetMaxWordsTest()
+        {
+            ConsoleApplication application = new ConsoleApplication();
+
+            string expected = "paw, wap, awp";
+
+            application.GroupWordsAndCount();
+
+            string actual = application.GetMaxWords();
+
+            Assert.AreEqual(expected, actual);
+
+        }
+
+       
+
+
+       
+        
+
+
+    }
+
+
+}
---
+++ Assignment 1/ConsoleAppTest/ConsoleAppTest/bin/Debug/ConsoleApplication.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleAppTest
+{
+    class ConsoleApplication
+    {
+       
+
+        //dictionalry holds the number of apparances in each word
+        Dictionary<string, int> dictionary =
+        new Dictionary<string, int>();
+
+        Dictionary<string, string> dictionaryWordHolder =
+        new Dictionary<string, string>();
+
+        // will hold original data
+        string[] wordArr = new string[] { "how", "who", "here", "paw", "wap", "awp" };
+
+        internal int Getcountof(string p)
+        {
+            if (dictionary.ContainsKey(p))
+                return dictionary[p];
+            else
+                return 0;
+        }
+
+        internal bool Insert(string key)
+        {
+            if (!dictionary.ContainsKey(key))
+                dictionary.Add(key, 1);
+            else
+                dictionary[key]++;
+
+  
+                return true;
+        }
+
+        //this method returns alphabeticaly sorted string
+        internal string GetAlphabaticalString(string p)
+        {
+            return String.Concat(p.OrderBy(c => c));
+
+        }
+
+        internal void AddToDictionaryWordHolder(string p)
+        {
+            string key = GetAlphabaticalString(p);
+
+            if (!dictionaryWordHolder.ContainsKey(key))
+                dictionaryWordHolder.Add(key, p);
+
+            else
+            {   
+                string newString = dictionaryWordHolder[key] + ", "  + p;
+
+                dictionaryWordHolder.Remove(key);
+
+                dictionaryWordHolder.Add(key, newString);
+
+            }
+        }
+
+        internal string GetfromDictionaryWordHolder(string p) 
+        {
+            string key = GetAlphabaticalString(p);
+
+            //System.Console.WriteLine("Hello World" + dictionaryWordHolder[key]);
+            return dictionaryWordHolder[key];
+        }
+
+        public void GroupWordsAndCount()
+        {
+            foreach (string t in wordArr)
+            {
+                AddToDictionaryWordHolder(t);
+
+                Insert(GetAlphabaticalString(t));
+            }
+        }
+
+        public string Run() {
+
+            GroupWordsAndCount();
+
+            Dictionary<string,int> tempDic = dictionary;
+
+            string last = "";
+
+            string tempKey;
+
+            foreach (var item in dictionary.OrderByDescending(key => key.Value))
+            {
+
+                tempKey = GetMaxKey();
+
+                last += dictionary[tempKey] + ": " + dictionaryWordHolder[tempKey] + "\n";
+
+                dictionary.Remove(tempKey);
+            }
+
+            dictionary = tempDic; //restoring original dictionary
+
+
+            return last;
+            
+        }
+
+        public string GetMaxKey() {
+
+            return dictionary.Aggregate((l, r) => l.Value > r.Value ? l : r).Key;
+        }
+
+        public string GetMaxWords() {
+
+            return dictionaryWordHolder[GetMaxKey()];
+        }
+
+
+        public int GetCount(string p) {
+
+            string key = GetAlphabaticalString(p);
+
+            if (dictionary.ContainsKey(key))
+                return dictionary[key];
+
+            else
+                return 0;
+        }
+        
+    }
+}
